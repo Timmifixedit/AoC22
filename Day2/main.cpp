dc5aa52fd3846d2561c755ce1ee88c8e1745db34
Timmifixedit <tim@luchterhand.org>
2022-12-07 22:48:18 +0100

    Day 2

---
+++ Day2/main.cpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <cassert>
+#include <iostream>
+#include "../util/util.hpp"
+
+
+struct RPSMove {
+    explicit constexpr RPSMove(char c) noexcept :  id(c), score(c - 'A' + 1) {
+        if (score > 3) {
+            score -= 23;
+        }
+    }
+
+    [[nodiscard]] constexpr char normalized() const noexcept {
+        if (id <= 'C') {
+            return id;
+        }
+
+        return static_cast<char>(id - 'X' + 'A');
+    }
+
+    [[nodiscard]] constexpr unsigned outcome(const RPSMove &other) const noexcept {
+        const char myId = normalized();
+        const char oId = other.normalized();
+        if (myId == oId) {
+            return 3;
+        }
+
+        if ((myId == 'A' && oId == 'C') || (myId == 'B' && oId == 'A') || (myId == 'C' && oId == 'B')) {
+            return 6;
+        }
+
+        return 0;
+    }
+
+    [[nodiscard]] constexpr RPSMove getResponse(const RPSMove &winCondition) const noexcept {
+        switch (winCondition.id) {
+            case 'X': {
+                int tmp = normalized() - 'A' - 1;
+                if (tmp < 0) {
+                    tmp += 3;
+                }
+                return RPSMove(static_cast<char>(tmp + 'A'));
+                break;
+            }
+            case 'Y':
+                return *this;
+            case 'Z':
+                return RPSMove(static_cast<char>((normalized() - 'A' + 1) % 3 + 'A'));
+            default:
+                abort();
+        }
+    }
+
+    [[nodiscard]] constexpr unsigned toOutcome() const noexcept {
+        return (id - 'X') * 3;
+    }
+
+    char id;
+    unsigned score;
+};
+
+
+int main(int argc, char **argv) {
+    auto file = util::getInputFile(argc, argv);
+    std::string line;
+    unsigned sumA1 = 0;
+    unsigned sumA2 = 0;
+    while (std::getline(file, line)) {
+        auto ret = util::splitString<' '>(line);
+        assert(ret.size() == 2);
+        RPSMove opponent(ret.front().front());
+        RPSMove me(ret.back().front());
+        auto resA1 = me.score + me.outcome(opponent);
+        auto response = opponent.getResponse(me);
+        auto resA2 = me.toOutcome() + response.score;
+        std::cout << ret.front() << " vs " << ret.back() << " = " << resA1
+                  << " | expected response: " << response.id << "(" << resA2 << ")" << std::endl;
+        sumA1 += resA1;
+        sumA2 += resA2;
+    }
+
+    std::cout << "total sum A1: " << sumA1 << std::endl;
+    std::cout << "total sum A2: " << sumA2 << std::endl;
+}
--- util/util.hpp
+++ util/util.hpp
@@ -6,21 +6,32 @@
 #define AOC22_UTIL_HPP
 
 #include <fstream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <sstream>
+
+
 namespace util {
+    namespace impl {
+        struct Word : std::string {};
+
+        template<char Delim>
+        std::istream &operator>>(std::istream &in, Word &word) {
+            std::getline(in, word, Delim);
+            return in;
+        }
+    }
     auto getInputFile(int argc, char** argv) noexcept -> std::fstream;
 
-    namespace traits {
-        template<typename Container, typename = std::void_t<>>
-        struct value_type {};
-
-        template<typename Container>
-        struct value_type<Container, std::void_t<decltype(*std::ranges::begin(std::declval<std::remove_cvref_t<Container>&>()))>> {
-            using type = std::remove_cvref_t<decltype(*std::ranges::begin(std::declval<std::remove_cvref_t<Container>&>()))>;
-        };
-
-        template<typename T>
-        using value_type_t = typename value_type<T>::type;
+    template<char Delim>
+    auto splitString(const std::string &string) -> std::vector<std::string> {
+        std::istringstream iss(string);
+        using iter = std::istream_iterator<impl::Word>;
+        std::vector<std::string> ret(iter{iss}, iter{});
+        return ret;
     }
+
 }
 
 #endif //AOC22_UTIL_HPP
