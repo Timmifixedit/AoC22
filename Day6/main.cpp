8a8fecb624654bd1e7d9e0f9e8f38d3f523e9272
Timmifixedit <tim@luchterhand.org>
2022-12-08 23:12:41 +0100

    day 6 riddle

---
+++ Day6/main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+#include <Iterators.hpp>
+#include <bidirectional_map.hpp>
+#include <cassert>
+#include "../util/util.hpp"
+
+
+int main() {
+    auto file = util::getInputFile();
+    std::string line;
+    std::getline(file, line);
+    bimap::bidirectional_map<char, std::size_t, std::unordered_map, std::map> markerChars;
+    auto &inverse = markerChars.inverse();
+    bool packageStarted = false;
+    for (auto [pos, c] : iterators::const_enumerate(line, 1)) {
+        if (markerChars.contains(c)) {
+            auto it = inverse.find(markerChars.at(c));
+            assert(it != inverse.end());
+            inverse.erase(inverse.begin(), ++it);
+        }
+
+        markerChars.emplace(c, pos);
+        if (markerChars.size() == 4 && !packageStarted) {
+            std::cout << "package marker at " << pos << std::endl;
+            packageStarted = true;
+        }
+
+        if (markerChars.size() == 14) {
+            std::cout << "message marker at " << pos << std::endl;
+            break;
+        }
+    }
+}
