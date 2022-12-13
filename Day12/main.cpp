e016203d1e061364e5aac0265d3d44afaaf0aa1f
Timmifixedit <tim@luchterhand.org>
2022-12-13 13:41:01 +0100

    Day 12 cleanup

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -22,18 +22,19 @@ public:
                 stride = line.size();
             }
 
-            for (auto [idx, c] : iterators::const_enumerate(line)) {
+            for (auto [i, c] : iterators::const_enumerate(line)) {
                 auto val = c - 'a';
+                auto idx = row * stride + i;
                 if (c == 'S') {
-                    start = row * stride + idx;
+                    start = idx;
                     val = 0;
                 } else if (c == 'E') {
-                    goal = row * stride + idx;
+                    goal = idx;
                     val = 'z' - 'a';
                 }
 
                 if (val == 0) {
-                    startingPoints.emplace_back(row * stride + idx);
+                    startingPoints.emplace_back(idx);
                 }
 
                 terrain.emplace_back(val);
@@ -61,18 +62,16 @@ public:
     std::vector<std::size_t> startingPoints;
 };
 
-
-
-template <typename T, std::totally_ordered P = unsigned>
+template <std::equality_comparable T>
 class SearchNode {
 public:
     using NodePtr = std::shared_ptr<SearchNode>;
-    template<typename T_, typename P_>
-    SearchNode(T_ state, NodePtr parent, P_ pathCost) : state(std::forward<T_>(state)), parent(std::move(parent)),
-                                                        pathCost(std::forward<P_>(pathCost)) {}
+    template<typename T_>
+    SearchNode(T_ state, NodePtr parent, unsigned pathCost) : state(std::forward<T_>(state)), parent(std::move(parent)),
+                                                              pathCost(pathCost) {}
 
     template<typename T_>
-    SearchNode(T_ state, NodePtr parent) : SearchNode(std::forward<T_>(state), std::move(parent), P{0}) {}
+    SearchNode(T_ state, NodePtr parent) : SearchNode(std::forward<T_>(state), std::move(parent), 0) {}
 
     bool operator==(const SearchNode &other) const {
         return state == other.state;
@@ -80,36 +79,46 @@ public:
 
     const T state;
     NodePtr parent;
-    P pathCost = 0;
+    unsigned pathCost = 0;
 };
 
-template<typename T, typename P>
-using NodePtr = typename SearchNode<T, P>::NodePtr;
+template<typename T>
+using NodePtr = std::shared_ptr<SearchNode<T>>;
 
-template<typename T, typename P, typename H>
+template<typename H, typename T>
+concept HeuristicFunction = requires(H instance, SearchNode<T> node) {
+    {instance(node.state)} -> std::totally_ordered;
+};
+
+
+template<typename T, HeuristicFunction<T> H>
 struct NodeCompare {
     template<typename H_>
     constexpr explicit NodeCompare(H_ && h) : h(std::forward<H_>(h)) {}
 
-    constexpr auto operator()(const NodePtr<T, P> &lhs, const NodePtr<T, P> &rhs) const {
+    constexpr auto operator()(const NodePtr<T> &lhs, const NodePtr<T> &rhs) const {
         return f(lhs) > f(rhs);
     }
 
 private:
-    [[nodiscard]] constexpr auto f(const NodePtr<T, P> &n) const {
+    [[nodiscard]] constexpr auto f(const NodePtr<T> &n) const {
         return n->pathCost + h(n->state);
     }
 
     H h;
 };
 
+template<typename E, typename T>
+concept ExpandFunction = requires(E instance, SearchNode<T> node) {
+    requires std::convertible_to<std::ranges::range_value_t<decltype(instance(node.state))>, T>;
+};
 
-template<typename T, typename P, typename ExpFun, typename HFun>
-auto aStar(const NodePtr<T, P> &start, const NodePtr<T, P> &goal, const ExpFun &expand, HFun &&h,
-           std::optional<P> upperBound) -> NodePtr<T, P> {
-    using NPtr = NodePtr<T, P>;
+template<typename T, ExpandFunction<T> ExpFun, HeuristicFunction<T> HFun>
+auto aStar(const NodePtr<T> &start, const NodePtr<T> &goal,
+           const ExpFun &expand, HFun &&h, std::optional<unsigned> upperBound) -> NodePtr<T> {
+    using NPtr = NodePtr<T>;
     std::vector<NPtr> visited;
-    NodeCompare<T, P, HFun> compare(std::forward<HFun>(h));
+    NodeCompare<T, HFun> compare(std::forward<HFun>(h));
     std::priority_queue fringe(compare, std::vector{start});
     while (not fringe.empty()) {
         auto current = std::move(const_cast<NPtr &>(fringe.top()));
@@ -130,9 +139,9 @@ auto aStar(const NodePtr<T, P> &start, const NodePtr<T, P> &goal, const ExpFun &
 
         visited.emplace_back(current);
         auto neighbours = expand(current->state);
-        for (const auto &n : neighbours) {
+        for (auto &n : neighbours) {
             auto g = current->pathCost + 1;
-            auto nPtr = std::make_shared<SearchNode<T, P>>(n, current, g);
+            auto nPtr = std::make_shared<SearchNode<T>>(std::move(n), current, g);
             fringe.emplace(std::move(nPtr));
         }
     }
@@ -141,13 +150,13 @@ auto aStar(const NodePtr<T, P> &start, const NodePtr<T, P> &goal, const ExpFun &
 }
 
 unsigned shortestPathToGoal(std::size_t start, const Field &field, std::optional<unsigned> upperBound = {}) {
-    auto startNode = std::make_shared<SearchNode<Field::HType>>(start, nullptr);
-    auto goalNode = std::make_shared<SearchNode<Field::HType>>(field.goal, nullptr);
+    auto startNode = std::make_shared<SearchNode<std::size_t>>(start, nullptr);
+    auto goalNode = std::make_shared<SearchNode<std::size_t>>(field.goal, nullptr);
     auto expand = [&field](std::size_t pos) {
         auto height = field.terrain[pos];
-        std::vector<std::size_t> neighbors;
-        neighbors.reserve(4);
         std::array candidates = {pos - 1, pos + 1, pos - field.stride, pos + field.stride};
+        std::vector<std::size_t> neighbors;
+        neighbors.reserve(candidates.size());
         for (auto c : candidates) {
             if (c < field.terrain.size() && field.terrain[c] - height <= 1) {
                 neighbors.emplace_back(c);
@@ -157,8 +166,7 @@ unsigned shortestPathToGoal(std::size_t start, const Field &field, std::optional
         return neighbors;
     };
 
-    auto res = aStar<Field::HType, unsigned>(startNode, goalNode, expand,
-                                             [&field](auto a) { return field.dist(a, field.goal); }, upperBound);
+    auto res = aStar(startNode, goalNode, expand, [&field](auto a) { return field.dist(a, field.goal); }, upperBound);
     if (res != nullptr) {
         return res->pathCost;
     }
@@ -177,7 +185,7 @@ int main(int argc, char **argv) {
     const Field field(file);
     auto shortest = shortestPathToGoal(field.start, field);
     std::cout << "ex 1: " << shortest << std::endl;
-    for (auto [idx, s] : iterators::enumerate(field.startingPoints)) {
+    for (auto s : field.startingPoints) {
         auto res = shortestPathToGoal(s, field, shortest);
         if (res < shortest) {
             std::cout << "new best " << res << std::endl;
